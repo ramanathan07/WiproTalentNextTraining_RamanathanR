3ec50a5efeb8cf522d69bbb524809f828ce67edd
ramanathan07 <89630494+ramanathan07@users.noreply.github.com>
2023-02-07 20:38:05 +0530

    Add files via upload

---
+++ day 2/loopprg01.java
@@ -0,0 +1,12 @@
+package basic1;
+
+import java.util.Scanner;
+
+public class loopprg01 {
+	public static void main(String[] args) {
+	       for(int i=1;i<=100;i++) {
+	    	   System.out.print(i+" ");
+	       }
+
+}
+} 
